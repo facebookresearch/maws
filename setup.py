df928532b417c82fc2a062d2696421b76fa6db5d
Miguel Martin <miguelmartin@fb.com>
2024-03-08 20:31:02 +0000

    Add setup.py to install as a pip package

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+
+from setuptools import find_packages, setup
+
+setup(
+    name="maws",
+    version="1.0.0",
+    author="FAIR",
+    description="Code and models for the paper \"The effectiveness of MAE pre-pretraining for billion-scale pretraining\" https://arxiv.org/abs/2303.13496",
+    url="https://github.com/facebookresearch/maws",
+    install_requires=[
+        "torch",
+        "torchvision",
+        "torchtext",
+        "timm==0.9.7",
+    ],
+    tests_require=[],
+    packages=find_packages(exclude=("tests", "tests.*")),
+)
